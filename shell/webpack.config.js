0b96e9afc2be32a06edf7d125e62e2bada920c16
Ramazan Özdemir <ramazanozdemir2607@gmail.com>
2025-05-05 23:25:05 +0200

    feat: webpoack dev and prod config was added

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -9,6 +11,30 @@ module.exports = {
     },
   },
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][hash][ext][query]",
+        },
+      },
+    ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
 };
