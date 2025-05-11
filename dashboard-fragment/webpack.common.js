4761ad05f83b73e1cc703b9efc049cb8d3af3ccc
Ramazan Ozdemir <ramazanozdemir2607@gmail.com>
2025-05-11 13:11:16 +0200

    feat: dashboard fragment was added

---
+++ dashboard-fragment/webpack.common.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const deps = require("./package.json").dependencies;
+require("dotenv").config();
+
+const path = require("path");
+
+const shared = {};
+for (const key in deps) {
+  shared[key] = {
+    singleton: true,
+    requiredVersion: deps[key],
+  };
+}
+
+module.exports = {
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][hash][ext][query]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new ModuleFederationPlugin({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Main": "./src/app.tsx",
+      },
+      shared,
+    }),
+  ],
+};
