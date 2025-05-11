6f53071c3bc9737da37f95d86595377fba1f261b
Ramazan Ozdemir <ramazanozdemir2607@gmail.com>
2025-05-11 08:53:56 +0200

    feat: fragement boilerplate was added

---
+++ fragment-boilerplate/webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(__dirname, "../dist"),
+    port: process.env.PORT,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+  },
+});
