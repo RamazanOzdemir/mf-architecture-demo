fc64e8e8e257041b9b855716924740671dc5c89b
Ramazan Ozdemir <ramazanozdemir2607@gmail.com>
2025-05-19 11:01:18 +0200

    feat: create recent orders table to dashboard

--- dashboard-fragment/src/app.tsx
+++ dashboard-fragment/src/app.tsx
@@ -1,12 +1,14 @@
-import RevenueChart from "./container/revenue-chart";
-import Stats from "./container/stats";
+import RecentOrders from "@/container/recent-orders";
+import RevenueChart from "@/container/revenue-chart";
+import Stats from "@/container/stats";
 import "./styles/globals.css";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 h-full">
+    <div className="flex flex-col gap-4 p-4  md:gap-6 md:p-6 h-full">
       <Stats />
       <RevenueChart />
+      <RecentOrders />
     </div>
   );
 }
---
+++ dashboard-fragment/src/container/recent-orders.tsx
@@ -0,0 +1,65 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const orders = [
+  {
+    id: "#1023",
+    customer: "Alice Y.",
+    status: "Completed",
+    amount: "$129.00",
+    date: "2025-05-17",
+  },
+  {
+    id: "#1022",
+    customer: "Bob T.",
+    status: "Pending",
+    amount: "$89.50",
+    date: "2025-05-16",
+  },
+  {
+    id: "#1021",
+    customer: "Clara J.",
+    status: "Cancelled",
+    amount: "$59.00",
+    date: "2025-05-15",
+  },
+];
+
+export default function RecentOrders() {
+  return (
+    <>
+      <h2 className="text-xl font-semibold mb-4">Recent Orders</h2>
+
+      <Table>
+        <TableCaption>A list of your recent orders.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Order ID</TableHead>
+            <TableHead>Customer</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {orders.map((order, index) => (
+            <TableRow key={`recent-orders-row-key${index}`}>
+              <TableCell className="font-medium">{order.id}</TableCell>
+              <TableCell>{order.customer}</TableCell>
+              <TableCell>{order.status}</TableCell>
+              <TableCell>{order.amount}</TableCell>
+              <TableCell className="text-right">{order.date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
